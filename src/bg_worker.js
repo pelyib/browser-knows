02c7878915d90ace6c446709ec8fce467e33acac
Botond Pelyi <botond.pelyi@gmail.com>
2023-07-15 07:21:12 +0200

    feat: install webextension-polyfill for cross browser support

--- src/bg_worker.js
+++ src/bg_worker.js
@@ -1,1 +1,27 @@
+import browser from "webextension-polyfill"
+
 console.log("hello on the backend")
+
+browser.runtime.onInstalled.addListener((details) => {
+   const currentVersion = chrome.runtime.getManifest().version
+   const previousVersion = details.previousVersion
+   const reason = details.reason
+   
+   console.log(`Previous Version: ${previousVersion }`)
+   console.log(`Current Version: ${currentVersion }`)
+
+   switch (reason) {
+      case 'install':
+         console.log('New User installed the extension.')
+         break;
+      case 'update':
+         console.log('User has updated their extension.')
+         break;
+      case 'chrome_update':
+      case 'shared_module_update':
+      default:
+         console.log('Other install events within the browser')
+         break;
+   }
+});
+
